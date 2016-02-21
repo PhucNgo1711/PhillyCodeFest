afd92c2c2c5322add327d2cbb3f4fed260ef98e8
PhucNgo1711 <phucnmrot@gmail.com>
2016-02-20 23:31:25 -0500

    Updated UI

--- js/controllers.js
+++ js/controllers.js
@@ -13,30 +13,134 @@ angular.module('app.controllers', [])
 })
 
 .controller('uploadCtrl', function($scope) {
+	$scope.postTitle = "";
 
+	$scope.ops = [ 
+		{ option: "Report" }, 
+		{ option: "Support" }, 
+		{ option: "Learn" } 
+	] ;
+
+	$scope.details = [ 
+		{ info: "Pothole" }, 
+		{ info: "Streetlight" }, 
+		{ info: "Law and Order" },
+		{ info: "Emergency 911"}
+	] ;
+
+	$scope.optionType = $scope.ops[0];
+	$scope.detail = $scope.details[0];
+
+	$scope.postContent = "Some Text Here";
+
+	$scope.optionChanged = function() {
+    	$scope.details = [ 
+		{ info: "Pet Adopting" }, 
+		{ info: "Hosp Volunteer" }, 
+		{ info: "Youth Mentoring" }
+		] ;
+
+		$scope.detail = $scope.details[0];
+   };
+
+	$scope.getPhoto = function() {
+    	console.log('Getting camera');
+    	Camera.getPicture({
+	      quality: 75,
+	      targetWidth: 320,
+	      targetHeight: 320,
+	      saveToPhotoAlbum: false
+    	}).then(function(imageURI) {
+      	console.log(imageURI);
+      	$scope.lastPhoto = imageURI;
+    	}, function(err) {
+      console.err(err);
+    	});
+  	}
+
+  	$scope.submit = function() {
+
+  	}
 })
 
 .controller('dashboardUserCtrl', function($scope) {
+	// $scope.username
+
 	$scope.searchText = "";
 
 	$scope.searchTextChanged = function () {
 
 	};
 
-	$scope.posts = [];
+	$scope.posts = [
+		{ "postID" : 1111, "userID" : 1234, "userName": "Phuc", "postTitle": "Foot", "postContent": "FootFoot Long String", "status": "Pending", "submissionTime": "02/20/2016 16:00:00", "attachment": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR-aYo5n3v2kW4MR3YD9E7sn77ljMJdr5fRVc-RSyU9nbF1tK9s" },
+		{ "postID" : 1111, "userID" : 1234, "userName": "Shaw", "postTitle": "Mouse", "postContent": "Mouse Long String", "status": "Solved", "submissionTime": "02/19/2016 10:00:00", "attachment": "http://www.colouringbook.org/random/colouringbook-1483.png" },
+		{ "postID" : 1111, "userID" : 1234, "userName": "Tess", "postTitle": "Rubic", "postContent": "Rubic Cube Long String", "status": "Pending", "submissionTime": "01/19/2016 10:00:00", "attachment": "http://zoarchurch.co.uk/content/pages/uploaded_images/91.png" },
+		{ "postID" : 1111, "userID" : 1234, "userName": "Vamsi", "postTitle": "Orange", "postContent": "Orange Long String", "status": "Solved", "submissionTime": "12/07/2015 10:00:00", "attachment": "http://www.clipartist.net/random/clipart-1046.png" },
+		{ "postID" : 1111, "userID" : 1234, "userName": "Tess", "postTitle": "Duck", "postContent": "Duck Long String", "status": "Pending", "submissionTime": "01/10/2015 10:00:00", "attachment": "http://www.colouringbook.org/random/colouringbook-860.png" }
+	];
+
+	$scope.summary = [];
+	$scope.timeAgo = [];
+
+	var summarizePosts = function() {
+		angular.forEach($scope.posts, function (post) {
+			s = post.postContent.substr(0, 20);
+
+			if (s.length >= 20) {
+				s += "...";
+			}
+
+			t = timeSince(post.submissionTime);
+			
+			$scope.summary.push(s);
+			$scope.timeAgo.push(t);
+		})
+	}	
+
+	function timeSince(submissionTime) {
+		var date = Date.parse(submissionTime);
+
+		var seconds = Math.floor((new Date() - date) / 1000);
+
+		var interval = Math.floor(seconds / 31536000);
+
+		if (interval > 1) {
+			return interval + " years";
+		}
+		interval = Math.floor(seconds / 2592000);
+		if (interval > 1) {
+			return interval + " months";
+		}
+		interval = Math.floor(seconds / 86400);
+		if (interval > 1) {
+			return interval + " days";
+		}
+		interval = Math.floor(seconds / 3600);
+		if (interval > 1) {
+			return interval + " hours";
+		}
+		interval = Math.floor(seconds / 60);
+		if (interval > 1) {
+			return interval + " minutes";
+		}
+		return Math.floor(seconds) + " seconds";
+	};
+
+	summarizePosts();
 })
 
 .controller('dashboardSponsorCtrl', function($scope) {
-
+	
 })
 
 .controller('profileCtrl', function($scope) {
 	$scope.name = "Phuc Ngo";
 
 	$scope.posts = [
-	{ "postID" : 1111, "userID" : 1234, "postName": "Rubic", "postContent": "Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube", "submissionTime": "02/20/2016 10:00:00", "attachment": "http://zoarchurch.co.uk/content/pages/uploaded_images/91.png" },
-	{ "postID" : 1111, "userID" : 1234, "postName": "Orange", "postContent": "Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube", "submissionTime": "01/07/2016 10:00:00", "attachment": "http://www.clipartist.net/random/clipart-1046.png" },
-	{ "postID" : 1111, "userID" : 1234, "postName": "Duck", "postContent": "Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube Rubic Cube", "submissionTime": "01/10/2015 10:00:00", "attachment": "http://www.colouringbook.org/random/colouringbook-860.png" }
+		{ "postID" : 1111, "userID" : 1234, "userName": "Phuc", "postTitle": "Rubic", "postContent": "Rubic Cube Long String", "status": "Pending", "submissionTime": "02/20/2016 10:00:00", "attachment": "http://zoarchurch.co.uk/content/pages/uploaded_images/91.png" },
+		{ "postID" : 1111, "userID" : 1234, "userName": "Phuc", "postTitle": "Orange", "postContent": "Orange Long String", "status": "Solved", "submissionTime": "01/07/2016 10:00:00", "attachment": "http://www.clipartist.net/random/clipart-1046.png" },
+		{ "postID" : 1111, "userID" : 1234, "userName": "Phuc", "postTitle": "Duck", "postContent": "DuckDuck Looong String", "status": "Solved", "submissionTime": "01/10/2015 10:00:00", "attachment": "http://www.colouringbook.org/random/colouringbook-860.png" }
 	];
 
 	$scope.numberOfPost = $scope.posts.length;
@@ -48,7 +152,12 @@ angular.module('app.controllers', [])
 
 	var summarizePosts = function() {
 		angular.forEach($scope.posts, function (post) {
-			s = post.postContent.substr(0, 20) + "...";
+			s = post.postContent.substr(0, 20);
+
+			if (s.length >= 20) {
+				s += "...";
+			}
+
 			t = timeSince(post.submissionTime);
 			
 			$scope.summary.push(s);
@@ -87,8 +196,79 @@ angular.module('app.controllers', [])
 
 	summarizePosts();
 
+	$scope.martial = [ 
+		{ info: "Single" }, 
+		{ info: "Married" } 
+	] ;
+
+	$scope.kids = [ 
+		{ info: "1" }, 
+		{ info: "2" }, 
+		{ info: "3" },
+		{ info: "4"}
+	] ;
+
+	$scope.social = [ 
+		{ info: "Sport" }, 
+		{ info: "Alcohol" }, 
+		{ info: "Hang Out" }
+	] ;
+
+	$scope.martialStatus = $scope.martial[0];
+	$scope.kid = $scope.kids[0];
+	$scope.socialPref = $scope.social[0];
+
 })
 
 .controller('notificationCtrl', function($scope) {
+	$scope.notifications = [
+		{ "postID" : 1111, "userID" : 1234, "userName": "Joseph", "notiContent": "Joseph solved your report", "submissionTime": "02/20/2016 10:00:00" },
+		{ "postID" : 1111, "userID" : 1234, "userName": "Joe", "notiContent": "Joe commented on your post", "submissionTime": "01/07/2016 10:00:00" },
+		{ "postID" : 1111, "userID" : 1234, "userName": "Joey", "notiContent": "Joey marked your post as duplicate", "submissionTime": "01/10/2015 10:00:00" }
+	];
 
+	$scope.summary = [];
+	$scope.timeAgo = [];
+
+	var summarizePosts = function() {
+		angular.forEach($scope.notifications, function (noti) {
+			s = noti.notiContent;
+
+			t = timeSince(noti.submissionTime);
+			
+			$scope.summary.push(s);
+			$scope.timeAgo.push(t);
+		})
+	}	
+
+	function timeSince(submissionTime) {
+		var date = Date.parse(submissionTime);
+
+		var seconds = Math.floor((new Date() - date) / 1000);
+
+		var interval = Math.floor(seconds / 31536000);
+
+		if (interval > 1) {
+			return interval + " years";
+		}
+		interval = Math.floor(seconds / 2592000);
+		if (interval > 1) {
+			return interval + " months";
+		}
+		interval = Math.floor(seconds / 86400);
+		if (interval > 1) {
+			return interval + " days";
+		}
+		interval = Math.floor(seconds / 3600);
+		if (interval > 1) {
+			return interval + " hours";
+		}
+		interval = Math.floor(seconds / 60);
+		if (interval > 1) {
+			return interval + " minutes";
+		}
+		return Math.floor(seconds) + " seconds";
+	}
+
+	summarizePosts();
 })
--- js/services.js
+++ js/services.js
@@ -1,7 +1,21 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
-
+.factory('Camera', ['$q', function($q) {
+ 
+  return {
+    getPicture: function(options) {
+      var q = $q.defer();
+      
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, options);
+      
+      return q.promise;
+    }
+  }
 }])
 
 .service('BlankService', [function(){
